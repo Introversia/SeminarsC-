df7c231ebefeeef03a21513a828a5c15a1dffa5f
Alena <alenastarwow@yandex.ru>
2024-04-12 18:03:10 +0400

    Seminar1 added comments to tasks

--- Seminars/Sem1/Program.cs
+++ Seminars/Sem1/Program.cs
@@ -1,11 +1,24 @@
-﻿
+﻿// Вывести значение переменной.
+
+// int a = 5;
+// Console.WriteLine(a);
+
+// Запросить значение переменной с помощью метода Console.ReadLine. Вычислить квадрат числа.
+// Чтобы конвертировать тип данных string в int, используем Convert.ToInt32.
+// Знак $ указывают для того чтобы подставить переменные напрямую и значения этих переменных
+// будут интегрированы внутрь текста.
+
 // Console.WriteLine("Input number: ");
 // int a = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine(a * a);
+// System.Console.WriteLine($"Quad of {a} -> {a * a}");
 
-// Console.WriteLine("Input first number: ");
+// Написать программу, которая на вход принимает два целых числа. Вычислить является ли первое число квадратом второго.
+// После условий и циклов не ставится знак " ; ". Знак " = " это присваивание значения переменной.
+// Знак " == " является оператором равенства.
+
+// Console.Write("Input first number: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input second number: ");
+// Console.Write("Input second number: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
 
 // if(num1 == num2 * num2)
@@ -17,23 +30,35 @@
 //     System.Console.WriteLine($"{num1} is not quad of {num2}"); 
 // }
 
-// Console.WriteLine("Input N: ");
+//  Написать программу, которая на вход принимает целое положительное число N, а на выходе показывает все целые числа от -N до N.
+//  Команда start++ прибавляет единицу, это тоже самое что и start = start + 1.
+
+// Console.Write("Input N: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 // int start = -n;
+
 // while (start <= n)
 // {
 //     System.Console.Write(start + " ");
 //     start++;
 // }
 
-Console.Write("Input three-digit number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+//  Напишите программу, которая принимает на вход трехзначное целое число и на выходе показывает сумму первой
+//  и последней цифры этого числа.
+//  Знак " && " является аналогом союза " и " и подразумевает что все эти условия должны быть выполнены.
+//  Знак " || " является аналогом союза " или " и подразумевает что как минимум одно из условий должно быть выполнено.
+//  Знак " % " это остаток от деления. Он позволяет отделить последнюю цифру трехзначного числа при делении на 10.
+//  Деление на 100 позволяет отделить первую цифру трехзначного числа.
+//  Если понадобится отделить цифру, находящуюся в середине, используется формула (num % 100) / 10.
 
-if(num >= 100 && num <= 999)
-{
-    int ed = num % 10;
-    int sot = num / 100;
-    System.Console.WriteLine($"Sum of {ed} and {sot} -> {ed + sot}");
-}
-else
-System.Console.WriteLine("You input not three-digit number");
+// Console.Write("Input three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num >= 100 && num <= 999)
+// {
+//     int ed = num % 10;
+//     int sot = num / 100;
+//     System.Console.WriteLine($"Sum of {ed} and {sot} -> {ed + sot}");
+// }
+// else
+// System.Console.WriteLine("You input not three-digit number");
--- Seminars/Sem1/obj/Debug/net8.0/Sem1.AssemblyInfo.cs
+++ Seminars/Sem1/obj/Debug/net8.0/Sem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59874b73fb9d3b5542e1103e8d66fd166b2bd63b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
