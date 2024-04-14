810b7e626e13d1fa6e7b7d8dea3974d3adfd97e5
Alena <alenastarwow@yandex.ru>
2024-04-14 10:17:03 +0400

    Mini changes

--- Homeworks/Sem1Homework1/Program.cs
+++ Homeworks/Sem1Homework1/Program.cs
@@ -2,174 +2,174 @@
 
 // 1. Напишите программу, которая на ввод принимает два числа и выводит какое число больше, а какое меньше.
 
-// using System;
+using System;
 
-// public class Answer {
-//     static void CompareNumbers(int firstNumber, int secondNumber)
-//     {
-//         // Введите свое решение ниже
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
 
-// if (firstNumber == secondNumber)
-// {
-//        Console.WriteLine("числа равны");
-// }
-// else if (firstNumber > secondNumber)
-// {
-//     Console.WriteLine("первое число больше");
-// }
-// else
-// {
-//     Console.WriteLine("второе число больше");
-// }
-//     }
+if (firstNumber == secondNumber)
+{
+       Console.WriteLine("числа равны");
+}
+else if (firstNumber > secondNumber)
+{
+    Console.WriteLine("первое число больше");
+}
+else
+{
+    Console.WriteLine("второе число больше");
+}
+    }
 
 
-//   // Не удаляйте и не меняйте метод Main! 
-//     static public void Main(string[] args) {
-//         int firstNumber, secondNumber;
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
 
-//         if (args.Length >= 2) {
-//             firstNumber = int.Parse(args[0]);
-//             secondNumber = int.Parse(args[1]);
-//         } else {
-//            // Здесь вы можете поменять значения для отправки кода на Выполнение
-//             firstNumber = 3;
-//             secondNumber = 6;
-//         }
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 3;
+            secondNumber = 6;
+        }
 
-//         // Не удаляйте строки ниже
-//         CompareNumbers(firstNumber, secondNumber);
-//     }
-// }
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
 
 
 
 //  2. Внутри класса Answer напишите метод FindMax, который принимает на вход три числа и выводит максимальное
 //  из этих чисел.
 
-// using System;
+using System;
 
-// public class Answer {
+public class Answer {
     
-//   static int FindMax(int a, int b, int c)
-//     {
-//       // Введите свое решение ниже
+  static int FindMax(int a, int b, int c)
+    {
+      // Введите свое решение ниже
 
-//     if (a >= b && a >= c)
-//     {
-//         return a;
-//     }
+    if (a >= b && a >= c)
+    {
+        return a;
+    }
 
-//     if (b >= a && b >= c)
-//     {
-//         return b;
-//     }
+    if (b >= a && b >= c)
+    {
+        return b;
+    }
 
-//     else
-//     {
-//         return c;
-//     }
-// }
+    else
+    {
+        return c;
+    }
+}
 
-//   // Не удаляйте и не меняйте метод Main! 
-//   static public void Main(string[] args) {
-//         int a, b, c;
+  // Не удаляйте и не меняйте метод Main! 
+  static public void Main(string[] args) {
+        int a, b, c;
 
-//         if (args.Length >= 3) {
-//            a = int.Parse(args[0]);
-//            b = int.Parse(args[1]);
-//            c = int.Parse(args[2]);
-//         } else {
-//            // Здесь вы можете поменять значения для отправки кода на Выполнение
-//            a = 8;
-//            b = 20;
-//            c = 7;
-//         }
+        if (args.Length >= 3) {
+           a = int.Parse(args[0]);
+           b = int.Parse(args[1]);
+           c = int.Parse(args[2]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+           a = 8;
+           b = 20;
+           c = 7;
+        }
 
-//         // Не удаляйте строки ниже
-//         int result = FindMax(a, b, c);
-//         System.Console.WriteLine($"{result}");
-//     }
-// }
+        // Не удаляйте строки ниже
+        int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
+    }
+}
 
 
 // 3. Напишите программу, которая принимает на ввод число и определяет, является ли оно четным.
 
-// using System;
+using System;
 
-// class Answer {
-//     static void CheckIfEven(int number)
-//     {
-//       // Введите свое решение ниже
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
 
-// if ((number % 2) == 0)
-// {
-//     Console.WriteLine("четное");
-// }
-// else
-// {
-//     Console.WriteLine("нечетное");
-// }
+if ((number % 2) == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
 
-//     }
+    }
   
 
-//   // Не удаляйте и не меняйте метод Main! 
-//     static public void Main(string[] args) {
-//         int number;
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
 
-//         if (args.Length >= 1) {
-//             number = int.Parse(args[0]);
-//         } else {
-//            // Здесь вы можете поменять значения для отправки кода на Выполнение
-//             number = 10;
-//         }
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 10;
+        }
 
-//         // Не удаляйте строки ниже
-//         CheckIfEven(number);
-//     }
-// }
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}
 
 
 
 // 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N включительно.
 
-// using System;
+using System;
 
-// public class Answer {
-//     static void PrintEvenNumbers(int number)
-//     {
-//       // Введите свое решение ниже
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
 
-// int i = 1;
+int i = 1;
 
-// while (i <= number)
+while (i <= number)
 
-//     if (i % 2 == 0)
-//     {
-//         Console.Write(i + " ");
-//         i++;
-//     }
-//     else
-//     {
-//         i++;
-//     }
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }
 
-//     }
+    }
 
 
-//   // Не удаляйте и не меняйте метод Main! 
-//     static public void Main(string[] args) {
-//         int number;
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
 
-//         if (args.Length >= 1) {
-//             number = int.Parse(args[0]);
-//         } else {
-//            // Здесь вы можете поменять значения для отправки кода на Выполнение
-//             number = 10;
-//         }
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 10;
+        }
 
-//         // Не удаляйте строки ниже
-//         PrintEvenNumbers(number);
-//     }
-// }  
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}  
