7882b9d0b767a86bdffc4305be01bfdf3e762b9a
Alena <alenastarwow@yandex.ru>
2024-04-17 09:12:12 +0400

    3rd Task of Sem4 added

--- Seminars/Sem4/Program.cs
+++ Seminars/Sem4/Program.cs
@@ -55,25 +55,62 @@
 
 // 2. Найдите количество чисел, которые заканчиваются на 1 и делятся нацело на 7.
 
-int NumCounter(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(array[i] % 10 == 1 && array[i] % 7 == 0) count++;
-    }
-    return count;
-}
-
-// int [] CreateArray(int size, int min, int max)
+// int NumCounter(int[] array)
 // {
-//     int[] array = new int[size];
-//     for (int i = 0; i < size; i++)
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
 //     {
-//         array[i] = new Random().Next(min, max+1);
+//         if(array[i] % 10 == 1 && array[i] % 7 == 0) count++;
+//     }
+//     return count;
+// }
+
+// // int [] CreateArray(int size, int min, int max)
+// // {
+// //     int[] array = new int[size];
+// //     for (int i = 0; i < size; i++)
+// //     {
+// //         array[i] = new Random().Next(min, max+1);
+// //     }
+// //     return array;
+// // }
+
+// int[] InputArray()
+// {
+//     Console.WriteLine("Input array size: ");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write("Input " + (i + 1) +" element: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
 //     }
 //     return array;
 // }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// // Console.WriteLine("Input array size: ");
+// // int size = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Input minimal value of array element: ");
+// // int min = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Input maximum value of array element: ");
+// // int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = InputArray();
+// PrintArray(array);
+// Console.WriteLine(NumCounter(array));
+
+
+// 3. Заполните массив случайных чисел от 1 до 9, который вводится с консоли.
+// Сформируйте целое число, которое будет состоять из цифр из массива.
 
 int[] InputArray()
 {
@@ -97,13 +134,16 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-// Console.WriteLine("Input array size: ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input minimal value of array element: ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input maximum value of array element: ");
-// int max = Convert.ToInt32(Console.ReadLine());
+int ConvertToNum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = result + array[i] * (int)Math.Pow(10, array.Length - 1 - i);
+    }
+    return result;
+}
 
 int[] array = InputArray();
 PrintArray(array);
-Console.WriteLine(NumCounter(array));
+System.Console.WriteLine(ConvertToNum(array));
