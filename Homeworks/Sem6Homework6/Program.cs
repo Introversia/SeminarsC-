8d87ff71837438e9185cf44fdc8f801920d87821
Alena <alenastarwow@yandex.ru>
2024-05-06 11:25:31 +0400

    Sem6Homework6 and Sem7Homework7 added

---
+++ Homeworks/Sem6Homework6/Program.cs
@@ -0,0 +1,78 @@
+﻿// 1. Задайте двумерный массив символов (тип char [,]). Создайте строку из символов этого массива.
+
+// string CharOfString(char[,] arr)
+// {
+//     string st = "";
+
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.WriteLine(i);
+//             st += arr[i, j];
+//         }
+//     }
+//     return st;
+// }
+
+
+// char[,] chars = new char[,]
+// {
+//     { 'a', '1' },
+//     { 'c', 'd' }
+// };
+// Console.WriteLine(CharOfString(chars));
+// CharOfString(chars);
+
+
+// 2. Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+// string text = "gdsKGF34!-0";
+// string result = text.ToLower();
+// Console.Write(result);
+
+
+// 3. Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+// using System.Text;
+// Console.InputEncoding = Encoding.Unicode;
+// Console.OutputEncoding = Encoding.Unicode;
+
+// bool IsPalindrom(string str)
+// {
+//     bool a = false;
+//     bool b = false;
+//     int count = 0;
+//     for (int i = 0; i < str.Length / 2; i++)
+//     {
+//         if (str[i] == str[str.Length - i - 1])
+//         {
+//             a = true;
+//             if (a)
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+//     if (count == str.Length / 2)
+//     {
+//         b = true;
+//     }
+//     return b;
+// }
+
+// void Print(bool str)
+// {
+//     if (str)
+//     {
+//         Console.WriteLine("Да");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Нет");
+//     }
+// }
+
+// string str = "окно";
+// bool isPalindrom = IsPalindrom(str);
+// Print(isPalindrom);
---
+++ Homeworks/Sem7Homework7/Program.cs
@@ -0,0 +1,56 @@
+﻿// 1. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+string PrintNaturalNumbers(int m, int n)
+{
+    if (m == n) return Convert.ToString(n);
+    return m + " " + PrintNaturalNumbers(m + 1, n);
+}
+
+Console.WriteLine("Введите число M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Натуральные числа от {m} до {n}:");
+Console.WriteLine(PrintNaturalNumbers(m, n));
+
+
+// 2. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int CalculateAkkermanFunction (int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0) 
+    {
+        return CalculateAkkermanFunction(m - 1, 1);
+    }
+    else 
+    {
+        return CalculateAkkermanFunction(m - 1, CalculateAkkermanFunction(m, n - 1));
+    }
+}
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({m}, {n}) = {CalculateAkkermanFunction(m, n)}");
+
+
+// 3. Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void ReverseArray (int[] array, int i)
+{
+    if(i < 0) return;
+    Console.Write(array[i] + " ");
+    ReverseArray(array, i - 1);
+}
+
+int[] array = {16, 10, 7, 1, 5};
+ReverseArray(array, array.Length - 1);
